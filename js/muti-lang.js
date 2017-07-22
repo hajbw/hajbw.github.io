a0070c4f0401601388b5353ca4cfc173ef1fb422
hajbw <hajbw10@126.com>
2017-07-22 23:34:34 +0800

    TODO:mutilang

---
+++ js/muti-lang.js
@@ -0,0 +1,18 @@
+var CHINESE=1
+var ENGLISH=2
+var UNDEFINED_LANGUAGE=0
+var DEFAULT_LANGUAGE=2
+
+function getNavigatorLanguage(){
+  
+}
+
+function setLanguage(languageCode){
+
+}
+
+
+
++function(){
+  setLanguage(getNavigatorLanguage())
+}
